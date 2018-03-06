61bda83633cd8b0343a1c3ed24d6a71308f9dacd
supratikchatterjee16 <supratikdevm96@gmail.com>
2018-03-06 08:07:18 +0530

    JS addition

--- pages/scripts/anima.js
+++ pages/scripts/anima.js
@@ -1,8 +1,24 @@
-var ctrl=0;
+var ctrl=[];
+var elements=[];
 function slideInLeft(div){
-    var doc = document.getElementById("leftpane");
-    alert(doc.clientWidth);
-    if(ctrl%2==0)doc.style.left+=doc.clientWidth;
-    else doc.style.left-=doc.clientWidth;
-    ctrl++;
+    var doc = document.getElementsByTagName(div)[0];
+    var pos=0;
+    while(pos<elements.length){
+        if(elements[pos]==doc.tagName)break;
+        pos++;
+    }
+    if(ctrl[pos]%2==0)doc.style.left=0;
+    else doc.style.left=("-"+doc.clientWidth+"pt");
+    ctrl[pos]++;
+}
+function test(){
+    var ls = document.body.getElementsByTagName("*");
+    for(var i=0;i<ls.length;i++){
+        //console.log(ls[i].tagName);
+        if(
+            ls[i].tagName!="BUTTON"&&
+            ls[i].tagName!="INPUT"
+        ){elements.push(ls[i].tagName);ctrl.push(0);}
+    }
+    elements.sort();
 }
